f17e39fed98db6bc4178ae3ca17f50c062ae8a2c
sarveshhome <sarvesh.carlton@gmail.com>
2020-05-09 12:12:29 +0530

    1 file modified Json product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-
+import { IProduct } from './IProduct';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,5 +7,25 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Product APP' // + VERSION.major;
+  
+
+  getProducts(): IProduct[]{
+      return 
+            [
+              {
+                "productId": 1,
+                "productName": "Brown eggs",
+                "productCode": "Pro-1",
+                "releaseDate": "March 31 2020",
+                "price": 30,
+                "description": "Raw organic brown eggs in a basket",
+                "starRating": 4,
+                "imageUrl": "assets/images/0.jpg"
+                }
+            ]
+                  
+      ;
+      
+  }
 
 }
