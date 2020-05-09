8d5700e3be1387a1080569877556f216d7495c8a
sarveshhome <sarvesh.carlton@gmail.com>
2020-05-09 12:21:02 +0530

    1 file modified Iproduct json 4 product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,40 @@ export class AppComponent  {
                 "price": 30,
                 "description": "Raw organic brown eggs in a basket",
                 "starRating": 4,
-                "imageUrl": "assets/images/0.jpg"
+                "imageUrl": "assets/images/1.jpg"
+                },
+			{
+                "productId": 2,
+                "productName": "Sweet fresh stawberry",
+                "productCode": "Pro-2",
+                "releaseDate": "March 21 2020",
+                "price": 29.45,
+                "description": "Sweet fresh stawberry on the wooden table",
+                "starRating": 4,
+                "imageUrl": "assets/images/2.jpg"
+                },
+				{
+                "productId": 3,
+                "productName": "Asparagus",
+                "productCode": "Pro-3",
+                "releaseDate": "March 31 2020",
+                "price": 18.95,
+                "description": "Asparagus with ham on the wooden table",
+                "starRating": 3,
+                "imageUrl": "assets/images/3.jpg"
+                },
+			{
+                "productId": 4,
+                "productName": "Green smoothie",
+                "productCode": "Pro-4",
+                "releaseDate": "March 23 2020",
+                "price": 17.68,
+                "description": "Glass of green smoothie with quail egg's yolk, served with cocktail tube, green apple and baby spinach leaves over tin surface.",
+                "starRating": 4,
+                "imageUrl": "assets/images/4.jpg"
                 }
-            ]
+				
+      ]
                   
       ;
       
